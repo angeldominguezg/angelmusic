1dda72057c4eb124f0c2b60ed441a64c341a9e12
Angelddg <angeldominguezg@gmail.com>
2017-08-20 08:34:55 -0400

    Initial commit

---
+++ src/api/config.js
@@ -0,0 +1,12 @@
+
+export default {
+    appname: 'AngelMusic',
+    apiKey: '2fcc1364d37080ed25e88538b6baa514',
+    secret: 'c5c06822050c4bc748c20ab96d137a21',
+    registeredTo: 'angelddg'
+}
+
+// Application name	AngelMusic
+// API key	2fcc1364d37080ed25e88538b6baa514
+// Shared secret	c5c06822050c4bc748c20ab96d137a21
+// Registered to	angelddg
